90ac91c2eb92e2d7f17611f4548c11109239ff72
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-04-11 21:24:53 +0700

    list, read, remove, update

--- controllers/product.js
+++ controllers/product.js
@@ -13,3 +13,56 @@ export const create = async (req, res) => {
         })
     }
 }
+// API list sản phẩm
+export const list = async (req, res) => { 
+    try {
+        const products = await Product.find().sort({createAt: -1});
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
+}
+// API list one sản phẩm
+export const read = async (req, res) => {
+    const condition = { _id: req.params.id}
+
+    try {
+        const product = await Product.findOne(condition);
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
+}
+// API xóa sản phẩm
+export const remove = async (req, res) => {
+    const condition = { _id: req.params.id}
+    try {
+        const product = await Product.findOneAndDelete(condition);
+        res.json({
+            message: "Đã xóa thành công",
+            data: product
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
+}
+// API cập nhật
+export const update = async (req, res) => {
+    const condition = { _id: req.params.id};
+    const doc = req.body;
+    const option = { new: true};
+    try {
+        const product = await Product.findOneAndUpdate(condition, doc, option);
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            message: "Lỗi không tìm được sản phẩm"
+        })
+    }
+}
