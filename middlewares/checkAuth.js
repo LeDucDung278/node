810da1ca45c66334b5c4cfa036299004c819d380
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-04-11 22:01:08 +0700

    checkAuth

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,24 @@
+import expressJWT from 'express-jwt';
+
+export const requireSignin = expressJWT({
+    algorithms: ["HS256"],
+    secret: "123456",
+    requestProperty: "auth" // req.auth    
+});
+export const isAuth = (req, res, next) => {
+    const status = req.profile._id == req.auth._id;
+    if(!status){
+        res.status(400).json({
+            message: "Ban khong co quyen truy cap"
+        })
+    }
+    next();
+}
+export const isAdmin = (req, res, next) => {
+    if(req.profile.role === 0){
+        res.status(401).json({
+            message: "Bạn không phải là admin"
+        })
+    }
+    next();
+}
