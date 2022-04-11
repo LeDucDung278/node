e93a3e8e3fc14bd8158178d06299f81c21db56da
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-04-11 22:16:12 +0700

    check mananer admin

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { create, list, read, remove, update } from '../controllers/product';
+import { userById } from '../controllers/user';
+import { isAdmin, isAuth, requireSignin } from '../middlewares/checkAuth';
 
 const router = Router()
 
-router.post('/products', create)
+router.post('/products/:userId', requireSignin, isAuth, isAdmin, create)
 router.get('/products',list)
 router.get('/products/:id', read);
-router.delete('/products/:id', remove);
-router.put("/products/:id", update);
+router.delete('/products/:id/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put("/products/:id/:userId", requireSignin, isAuth, isAdmin, update);
 
+router.param("userId", userById)
 
 export default router
